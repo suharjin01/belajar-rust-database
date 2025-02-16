d37cc5c1e22f7eeb650ade749dd59eb3d3075ad1
Suharjin <suharjin01@gmail.com>
2025-02-16 21:53:55 +0800

    Connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+#[cfg(test)]
+mod tests {
+    use sqlx::{Connection, Error, PgConnection};
+
+    
+    #[tokio::test]
+    async fn test_manual_connection() -> Result<(), Error> {
+        let url = "postgres://suharjin:@localhost:5432/belajar_rust";
+        let connection: PgConnection = PgConnection::connect(url).await?;
+
+        connection.close().await?;
+        Ok(())
+    }
+}
