a3379cdbc118a530a0c96b9fe1665dfffb99ec01
Suharjin <suharjin01@gmail.com>
2025-02-18 20:16:53 +0800

    Database Pool

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, Error, PgConnection};
+    use std::time::Duration;
+
+    use sqlx::{postgres::PgPoolOptions, Connection, Error, PgConnection, Pool, Postgres};
 
     
     #[tokio::test]
@@ -15,5 +17,24 @@ mod tests {
 
         connection.close().await?;
         Ok(())
+    }
+
+
+    // Database Pool
+    #[tokio::test]
+    async fn test_pool_connection() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        pool.close().await;
+        Ok(())
+    }
+
+    async fn get_pool() -> Result<Pool<Postgres>, Error> {
+        let url = "postgres://suharjin:@localhost:5432/belajar_rust";
+        PgPoolOptions::new()
+            .max_connections(10)
+            .min_connections(5)
+            .acquire_timeout(Duration::from_secs(5))
+            .idle_timeout(Duration::from_secs(60))
+            .connect(url).await
     }
 }
